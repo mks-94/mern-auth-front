0d77dda0689da92e98fd62e31d4a343f26aa7ad0
mks-94 <muralikrishna9424.com>
2021-01-02 11:16:19 +0530

    Implemented header and inside it nav button links.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Header from "./components/layouts/Header";
 import Home from "./components/pages/Home";
 
 const App = () => {
   return (
     <>
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
