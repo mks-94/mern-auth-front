d2a41cfaf1d0246161a83c0597ced725f354ff50
mks-94 <muralikrishna9424.com>
2021-01-03 21:36:29 +0530

    Implemented error handling

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -2,32 +2,41 @@ import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import UserContext from "../../context/UserContext";
 import Axios from "axios";
+import ErrorNotice from "../misc/ErrorNotice";
 
 const Login = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [error, setError] = useState();
 
   const { setUserData } = useContext(UserContext);
   const history = useHistory();
 
   const submit = async (e) => {
     e.preventDefault();
-    const loginUser = { email, password };
-    const loginRes = await Axios.post(
-      "http://localhost:5000/users/login",
-      loginUser
-    );
-    setUserData({
-      token: loginRes.data.token,
-      user: loginRes.data.user,
-    });
-    localStorage.setItem("auth-token", loginRes.data.token);
-    history.push("/");
+    try {
+      const loginUser = { email, password };
+      const loginRes = await Axios.post(
+        "http://localhost:5000/users/login",
+        loginUser
+      );
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/");
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+    }
   };
 
   return (
     <div className="page">
       <h2>Login</h2>
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
       <form className="form" onSubmit={submit}>
         <label htmlFor="login-email">Email:</label>
         <input
